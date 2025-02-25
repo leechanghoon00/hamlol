0d2b0ccc8d5b3d94c77daebf260e18dfb5f924b0
leechanghoon00 <ddong9124@gmail.com>
2025-02-25 21:07:32 +0900

    1

---
+++ src/main/java/org/example/hamlol/HamlolApplication.java
@@ -0,0 +1,13 @@
+package org.example.hamlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamlolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamlolApplication.class, args);
+    }
+
+}

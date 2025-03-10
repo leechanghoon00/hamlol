aae4f673f772150888532893e8b7c0ea152ac6de
leechanghoon00 <ddong9124@gmail.com>
2025-03-10 09:51:26 +0900

    회원가입한 아이디와 롤 아이디 연동 기능 추가, 서비스 기능 코드들 왜 썻는지 주석 달기

--- src/main/java/org/example/hamlol/service/impl/AccountServiceImpl.java
+++ src/main/java/org/example/hamlol/service/impl/AccountServiceImpl.java
@@ -37,6 +37,7 @@ public class AccountServiceImpl implements AccountService {
     public AccountServiceImpl(AccountRepository accountRepository, RestTemplate restTemplate) {
         this.accountRepository = accountRepository; //전달받은 accountRepository를 인스턴스를 클래스의 필드로 할등
         this.restTemplate = restTemplate;
+
     }
 
 
@@ -44,7 +45,7 @@ public class AccountServiceImpl implements AccountService {
     // AccountRequestDto를 받아 Riotapi로부터 계정정보 조회후 저장하고  AccountResponseDTO 형태로 반환
     public AccountResponseDTO getAccountInfoAndSaveAccount(AccountRequestDto accountRequestDto) throws Exception {  
 // throws Exception : 메소드 실행 도중 발생할 수 있는 예외를 전달함
-        String apiKey = "라이엇키";  // Riot API 키
+        String apiKey = "RGAPI-3c7e9c31-115d-4ba2-85d2-1fe9e61935a1";  // Riot API 키
 
         // URI 빌더를 사용하여 Riot API 요청 URL 생성 (경로 변수 적용)
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.
@@ -77,6 +78,7 @@ public class AccountServiceImpl implements AccountService {
             accountEntity.setPuuid(generatedPuuid);
             accountEntity.setGameName(accountRequestDto.gameName());
             accountEntity.setTagLine(accountRequestDto.tagLine());
+            accountEntity.setUserEntity(user);
 
 
 

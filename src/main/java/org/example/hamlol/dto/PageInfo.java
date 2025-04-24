e4f6361f82338cd58d383994da8dbcbacc8cfb6c
leechanghoon00 <ddong9124@gmail.com>
2025-04-24 14:40:32 +0900

    전적 더보기 버튼 기능 추가

--- src/main/java/org/example/hamlol/dto/PageInfo.java
+++ src/main/java/org/example/hamlol/dto/PageInfo.java
@@ -1,16 +1,17 @@
 package org.example.hamlol.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class PageInfo {
-    private final int totalPosts;      // 모든 글 개수
-    private final int currentPage;     // 현재 페이지 번호
-    private final int postsPerPage;    // 한 페이지당 표시할 글 개수
-    private final int displayPageNum;  // 한 번에 표시할 페이지 개수
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
 
 }

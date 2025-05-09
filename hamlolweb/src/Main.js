536d6b6b1c976e9d1feb76d750f659e228f43290
leechanghoon00 <ddong9124@gmail.com>
2025-05-09 11:52:32 +0900

    [fix] 메인에서 로그아웃 할 때 로그인창으로 가게 변경

--- hamlolweb/src/Main.js
+++ hamlolweb/src/Main.js
@@ -8,7 +8,7 @@ function Main() {
             <p>로그인에 성공했습니다.</p>
             <p>이곳은 메인 페이지입니다.</p>
             <div className="link">
-                <Link to="/logout">로그아웃</Link>
+                <Link to="/login">로그아웃</Link>
                 <Link to="/savegame">전적 등록하기</Link>
                 <Link to="/account">계정 연동하기</Link>
                 <Link to="/gameList">전적 보기</Link>

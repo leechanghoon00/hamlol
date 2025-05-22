8b25b00795acc773c0cf112fd0b2c217f74fbf01
ryu1002 <ddong9124@gmail.com>
2025-05-22 17:18:11 +0900

    1

--- src/main/java/org/example/hamlol/HamlolApplication.java
+++ src/main/java/org/example/hamlol/HamlolApplication.java
@@ -2,12 +2,18 @@ package org.example.hamlol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HamlolApplication {
+public class HamlolApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HamlolApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HamlolApplication.class, args);
     }
-
 }

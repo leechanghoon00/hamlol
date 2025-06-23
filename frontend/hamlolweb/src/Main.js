03c771721daae67fd513fe289c4e95d40bd2b1c7
ryu1002 <ddong9124@gmail.com>
2025-06-23 11:18:22 +0900

    CI 테스트용 커밋

--- frontend/hamlolweb/src/Main.js
+++ frontend/hamlolweb/src/Main.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+
 function Main() {
+    const [userName, setUserName] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken"); // GameList.js와 통일
+        if (token) {
+            const decoded = jwtDecode(token);
+            setUserName(decoded.user_name); // 서버 JWT 구조가 이렇게 되어 있어야 함
+        }
+    }, []);
+
     return (
         <div className="main-container">
-            <h1>환영합니다!</h1>
-            {/*<p>로그인에 성공했습니다.</p>*/}
-            {/*<p>이곳은 메인 페이지입니다.</p>*/}
+            <h1>{userName ? `${userName} 님, 환영합니다!` : "환영합니다!"}</h1>
             <div className="link">
                 <Link to="/login">로그아웃</Link>
                 <Link to="/savegame">전적 등록하기</Link>

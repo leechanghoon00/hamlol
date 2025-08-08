74b13b6b11621d9eeb404ddd1db3102c0917d850
ryu1002 <ddong9124@gmail.com>
2025-08-08 13:28:59 +0900

    1

--- frontend/hamlolweb/src/Login.js
+++ frontend/hamlolweb/src/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import {useNavigate} from "react-router-dom";
-import { Link } from "react-router-dom";
 
 function Main(){
     const navigate = useNavigate();
